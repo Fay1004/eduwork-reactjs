a8ce2b96cb38baaadc600b0cf88d1ff1896aa515
Agung Rifai <agungfay10@gmail.com>
2023-02-18 09:27:49 +0700

    maning

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
-          Di Coba
+          Di Coba Maning Lah
         </p>
         <a
           className="App-link"
